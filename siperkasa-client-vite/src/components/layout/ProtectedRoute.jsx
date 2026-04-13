dd2e624dec5e15ba4c2b3e7a3dedd00b50665655
unknown <wadangalap@gmail.com>
2026-04-13 11:34:17 +0700

    tes spdp routess

--- siperkasa-client-vite/src/components/layout/ProtectedRoute.jsx
+++ siperkasa-client-vite/src/components/layout/ProtectedRoute.jsx
@@ -1,11 +1,24 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function ProtectedRoute() {
-  const token = localStorage.getItem("access_token");
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState(null);
 
+  useEffect(() => {
+    const t = localStorage.getItem("access_token");
+    setToken(t);
+    setLoading(false);
+  }, []);
+
+  // 🔥 tahan dulu biar gak redirect prematur
+  if (loading) return null;
+
+  // ❌ belum login
   if (!token || token === "undefined") {
     return <Navigate to="/" replace />;
   }
 
+  // ✅ aman
   return <Outlet />;
 }
